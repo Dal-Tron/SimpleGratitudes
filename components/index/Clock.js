a84b60ec617b9160c345d9bbf638b799f3bdda99
Dallas Hermanson <inboxDallas@gmail.com>
2019-05-26 01:40:26 -0700

    try all new changes

--- components/index/Clock.js
+++ components/index/Clock.js
@@ -1,13 +1,13 @@
 export default props => <div
   style={{
-    background: '#7eb8cb'
+    background: "#7eb8cb"
   }}>
   <div>
     <div style={{
-      textAlign: 'center',
-      fontSize: '20px',
-      fontFamily: 'Snippet, sans-serif',
-      color: 'white',
+      textAlign: " center",
+      fontSize: "20px",
+      fontFamily: "Snippet, sans-serif",
+      color: "white",
     }}>
       <div>{props.timestring}</div>
     </div>
