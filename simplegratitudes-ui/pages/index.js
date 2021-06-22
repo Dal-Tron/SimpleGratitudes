97c1b4b783348e23419568d906228225153c0d25
Dallas Hermanson <inboxDallas@gmail.com>
2021-06-22 16:29:10 -0700

    init

---
+++ simplegratitudes-ui/pages/index.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import Head from 'next/head'
+import dayjs from 'dayjs'
+import { SmileTwoTone } from '@ant-design/icons'
+
+export default function Home() {
+  const getTime = () => dayjs().format('h:mm A MMMM D, YYYY');
+  const [time, setTime] = useState(() => getTime());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(() => getTime());
+    }, 10000);
+
+    return () => clearInterval(timer);
+  });
+
+  return (
+    <div className='wrapper'>
+      <Head>
+        <title>Simple Gratitudes</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+        />
+      </Head>
+      <section className="headspace">
+        <div className='intention'>
+          <div>Simple</div>
+          <div>Gratitudes</div>
+        </div>
+        <div className='avatar'>
+          <SmileTwoTone twoToneColor='#73b8cb' />
+        </div>
+      </section>
+      <section className="time">
+        {time}
+      </section>
+      <section className="container">
+        Hello
+      </section>
+    </div >
+  )
+}
