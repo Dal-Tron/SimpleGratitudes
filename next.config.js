743e9ab990308674d7884b7cc8bf246e29875765
Dallas Hermanson <inboxDallas@gmail.com>
2021-09-20 17:34:36 -0700

    modified next.config removed react-env

--- next.config.js
+++ next.config.js
@@ -1,14 +1,38 @@
 const path = require('path')
 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  env: {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
-  },
-  images: {
-    domains: ['hedciaofaszacfhqwvky.supabase.co']
+module.exports = (phase, { defaultConfig }) => {
+  let SUPABASE_URL, SUPABASE_SERVICE_KEY;
+
+  switch (process.env.ENV) {
+    case "development":
+      SUPABASE_URL = process.env.DEVELOPMENT_SUPABASE_URL;
+      SUPABASE_SERVICE_KEY = process.env.DEVELOPMENT_SUPABASE_SERVICE_KEY;
+      break;
+    case "staging":
+      SUPABASE_URL = process.env.STAGING_SUPABASE_URL;
+      SUPABASE_SERVICE_KEY = process.env.STAGING_SUPABASE_SERVICE_KEY;
+      break;
+    case "production":
+      SUPABASE_URL = process.env.PRODUCTION_SUPABASE_URL;
+      SUPABASE_SERVICE_KEY = process.env.PRODUCTION_SUPABASE_SERVICE_KEY;
+      break;
+    default:
+      SUPABASE_URL = process.env.DEVELOPMENT_SUPABASE_URL;
+      SUPABASE_SERVICE_KEY = process.env.DEVELOPMENT_SUPABASE_SERVICE_KEY;
+      break;
+  }
+
+  return {
+    ...defaultConfig,
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+    },
+    env: {
+      SUPABASE_URL,
+      SUPABASE_SERVICE_KEY,
+    },
+    images: {
+      domains: ['hedciaofaszacfhqwvky.supabase.co']
+    }
   }
 }
