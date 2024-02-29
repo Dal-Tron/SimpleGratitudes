eb5bd73e7281fcd6ba24152a388628ce1a7eef22
inboxDallas <inboxdallas@gmail.com>
2024-02-29 11:59:42 -0800

    chore: adding more icons, check, close, trash

---
+++ icons/Close.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+
+export const CloseIcon: FC<{ className?: string }> = ({ className }) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M6 18L18 6M6 6l12 12"
+        stroke="currentColor"
+      />
+    </svg>
+  );
+};
