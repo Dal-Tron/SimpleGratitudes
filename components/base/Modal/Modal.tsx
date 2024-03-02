8cb3191ef0430a8bda74c234ebf5155a37fc2e1c
inboxDallas <inboxdallas@gmail.com>
2024-03-02 11:23:38 -0800

    fix: modal needed a stop propagation

--- components/base/Modal/Modal.tsx
+++ components/base/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { FC, ReactNode } from 'react';
+import { FC, MouseEvent, ReactNode } from 'react';
 
 import { Button } from '@/components/base/Button/Button';
 import { CloseIcon } from '@/icons/Close';
@@ -21,6 +21,10 @@ export const Modal: FC<ModalProps> = ({
     return null;
   }
 
+  const handleModalContentClick = (e: MouseEvent) => {
+    e.stopPropagation(); // Prevent click from propagating to the backdrop
+  };
+
   return (
     <div
       onClick={onClose}
@@ -28,7 +32,10 @@ export const Modal: FC<ModalProps> = ({
       id="modal"
     >
       <div className="flex items-center justify-center h-full w-full -mt-20">
-        <div className={clsx('relative rounded-md w-96 p-5', className)}>
+        <div
+          className={clsx('relative rounded-md w-96 p-5', className)}
+          onClick={handleModalContentClick}
+        >
           <Button
             className="m-4 text-gray-400 absolute top-0 right-0 z-10"
             onClick={onClose}
