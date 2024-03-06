428d8755f4e8cc656631c86c672d9fa9c148fb0a
inboxDallas <inboxdallas@gmail.com>
2024-03-06 10:10:32 -0800

    feat: change sign in and register to use loading wheel

--- components/feature/SignModal/RegisterTab.tsx
+++ components/feature/SignModal/RegisterTab.tsx
@@ -5,6 +5,8 @@ import { ChangeEvent, FC, useState } from 'react';
 import { Button } from '@/components/base/Button/Button';
 import { AuthService } from '@/services/auth';
 
+import { SpinLoading } from '@/components/base/Loading/SpinLoading';
+import { validEmail, validPassword } from '@/helpers/validation';
 import { SignIn } from './SignIn';
 
 interface RegisterTabProps {
@@ -27,11 +29,16 @@ export const RegisterTab: FC<RegisterTabProps> = ({
   const router = useRouter();
 
   const [loading, setLoading] = useState(false);
+  const [checkValidation, setCheckValidation] = useState(false);
+
   const handleChangeTab = () => {
     onChangeTab('1');
   };
 
   const handleSubmit = async () => {
+    setCheckValidation(true);
+    if (!validEmail(email) || !validPassword(password)) return;
+
     setLoading(true);
 
     try {
@@ -78,48 +85,57 @@ export const RegisterTab: FC<RegisterTabProps> = ({
   return (
     <>
       <SignIn
-        key="register-signin"
+        checkValidation={checkValidation}
         disabled={loading}
         email={email}
+        key="register"
         password={password}
         setEmail={handleEmailChange}
         setPassword={handlePasswordChange}
       />
-      <div className="user-modal-no-account-wrapper">
-        <div className="user-modal-no-account-question">
-          Already have an account?
+      {loading ? (
+        <div className="flex justify-center p-6">
+          <SpinLoading className="text-5xl" />
         </div>
-        <div
-          onClick={handleChangeTab}
-          className="user-modal-no-account-register"
-        >
-          Sign In
-        </div>
-      </div>
-      <div className="flex flex-row justify-end gap-2">
-        <Button onClick={onCancel} type="secondary">
-          Cancel
-        </Button>
-        <Button onClick={handleSubmit} type="primary">
-          {loading ? 'Registering' : 'Register'}
-        </Button>
-      </div>
-      <div className="flex flex-col justify-end gap-2 mt-6">
-        <Button
-          onClick={handleSubmitGmail}
-          type="custom"
-          primaryColor="#df4936"
-        >
-          {loading ? 'Registering with Gmail' : 'Register with Gmail'}
-        </Button>
-        <Button
-          onClick={handleSubmitFacebook}
-          type="custom"
-          primaryColor="#3a5a8d"
-        >
-          {loading ? 'Registering with Facebook' : 'Register with Facebook'}
-        </Button>
-      </div>
+      ) : (
+        <>
+          <div className="user-modal-no-account-wrapper">
+            <div className="user-modal-no-account-question">
+              Already have an account?
+            </div>
+            <div
+              onClick={handleChangeTab}
+              className="user-modal-no-account-register"
+            >
+              Sign In
+            </div>
+          </div>
+          <div className="flex flex-row justify-end gap-2">
+            <Button onClick={onCancel} type="secondary">
+              Cancel
+            </Button>
+            <Button onClick={handleSubmit} type="primary">
+              Register
+            </Button>
+          </div>
+          <div className="flex flex-col justify-end gap-2 mt-6">
+            <Button
+              onClick={handleSubmitGmail}
+              type="custom"
+              primaryColor="#df4936"
+            >
+              Register with Gmail
+            </Button>
+            <Button
+              onClick={handleSubmitFacebook}
+              type="custom"
+              primaryColor="#3a5a8d"
+            >
+              Register with Facebook
+            </Button>
+          </div>
+        </>
+      )}
     </>
   );
 };
--- components/feature/SignModal/SignInTab.tsx
+++ components/feature/SignModal/SignInTab.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/base/Button/Button';
 import { validEmail, validPassword } from '@/helpers/validation';
 import { AuthService } from '@/services/auth';
 
+import { SpinLoading } from '@/components/base/Loading/SpinLoading';
 import { SignIn } from './SignIn';
 
 interface SignInTabProps {
@@ -83,49 +84,57 @@ export const SignInTab: FC<SignInTabProps> = ({
   return (
     <>
       <SignIn
-        key="password-signin"
+        checkValidation={checkValidation}
         disabled={loading}
         email={email}
+        key="signin"
         password={password}
         setEmail={handleEmailChange}
         setPassword={handlePasswordChange}
-        checkValidation={checkValidation}
       />
-      <div className="user-modal-no-account-wrapper">
-        <div className="user-modal-no-account-question">
-          Don't have an account?
+      {loading ? (
+        <div className="flex justify-center p-6">
+          <SpinLoading className="text-5xl" />
         </div>
-        <div
-          onClick={handleChangeTab}
-          className="user-modal-no-account-register"
-        >
-          Register
-        </div>
-      </div>
-      <div className="flex flex-row justify-end gap-2">
-        <Button onClick={onCancel} type="secondary">
-          Cancel
-        </Button>
-        <Button onClick={handleSubmit} type="primary">
-          {loading ? 'Signing In' : 'Sign In'}
-        </Button>
-      </div>
-      <div className="flex flex-col justify-end gap-2 mt-6">
-        <Button
-          onClick={handleSubmitGmail}
-          type="custom"
-          primaryColor="#df4936"
-        >
-          {loading ? 'Signing In with Gmail' : 'Sign In with Gmail'}
-        </Button>
-        <Button
-          onClick={handleSubmitFacebook}
-          type="custom"
-          primaryColor="#3a5a8d"
-        >
-          {loading ? 'Signing In with Facebook' : 'Sign In with Facebook'}
-        </Button>
-      </div>
+      ) : (
+        <>
+          <div className="user-modal-no-account-wrapper">
+            <div className="user-modal-no-account-question">
+              Don't have an account?
+            </div>
+            <div
+              onClick={handleChangeTab}
+              className="user-modal-no-account-register"
+            >
+              Register
+            </div>
+          </div>
+          <div className="flex flex-row justify-end gap-2">
+            <Button onClick={onCancel} type="secondary">
+              Cancel
+            </Button>
+            <Button onClick={handleSubmit} type="primary">
+              Sign In
+            </Button>
+          </div>
+          <div className="flex flex-col justify-end gap-2 mt-6">
+            <Button
+              onClick={handleSubmitGmail}
+              type="custom"
+              primaryColor="#df4936"
+            >
+              Sign In with Gmail
+            </Button>
+            <Button
+              onClick={handleSubmitFacebook}
+              type="custom"
+              primaryColor="#3a5a8d"
+            >
+              Sign In with Facebook
+            </Button>
+          </div>
+        </>
+      )}
     </>
   );
 };
